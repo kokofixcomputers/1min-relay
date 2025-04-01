76b6f00d6e1737e637de6d0e5d907e9d16afc878
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-04-01 19:23:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1275,7 +1275,7 @@ def conversation():
                             "index": 0,
                             "message": {
                                 "role": "assistant", 
-                                "content": f"🔊 [Аудио сообщение]({full_audio_url})"
+                                "content": f"🔊 [Audio file]({full_audio_url})"
                             },
                             "finish_reason": "stop"
                         }
