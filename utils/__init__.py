5056ca46e302efd817da4bd3c42429070abb1ff2
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-04-04 20:03:07 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,9 +3,28 @@ from utils.common import (
     safe_temp_file, ERROR_HANDLER, handle_options_request, split_text_for_streaming
 )
 from utils.memcached import check_memcached_connection, safe_memcached_operation, delete_all_files_task
+from utils.constants import *
 
 __all__ = [
+    # Common functions
     'calculate_token', 'api_request', 'set_response_headers', 'create_session',
     'safe_temp_file', 'ERROR_HANDLER', 'handle_options_request', 'split_text_for_streaming',
-    'check_memcached_connection', 'safe_memcached_operation', 'delete_all_files_task'
+    
+    # Memcached functions
+    'check_memcached_connection', 'safe_memcached_operation', 'delete_all_files_task',
+    
+    # Constants
+    'ONE_MIN_API_URL', 'ONE_MIN_ASSET_URL', 'ONE_MIN_CONVERSATION_API_URL', 
+    'ONE_MIN_CONVERSATION_API_STREAMING_URL', 'DEFAULT_TIMEOUT', 'MIDJOURNEY_TIMEOUT',
+    'IMAGE_GENERATOR', 'IMAGE_VARIATOR', 'IMAGE_DESCRIPTION_INSTRUCTION', 
+    'DOCUMENT_ANALYSIS_INSTRUCTION', 'PORT', 'ALL_ONE_MIN_AVAILABLE_MODELS',
+    'VISION_SUPPORTED_MODELS', 'CODE_INTERPRETER_SUPPORTED_MODELS', 
+    'RETRIEVAL_SUPPORTED_MODELS', 'FUNCTION_CALLING_SUPPORTED_MODELS',
+    'IMAGE_GENERATION_MODELS', 'VARIATION_SUPPORTED_MODELS',
+    'IMAGE_VARIATION_MODELS', 'MIDJOURNEY_ALLOWED_ASPECT_RATIOS',
+    'FLUX_ALLOWED_ASPECT_RATIOS', 'LEONARDO_ALLOWED_ASPECT_RATIOS',
+    'DALLE2_SIZES', 'DALLE3_SIZES', 'LEONARDO_SIZES', 'ALBEDO_SIZES',
+    'TEXT_TO_SPEECH_MODELS', 'SPEECH_TO_TEXT_MODELS',
+    'SUBSET_OF_ONE_MIN_PERMITTED_MODELS', 'PERMIT_MODELS_FROM_SUBSET_ONLY',
+    'MAX_CACHE_SIZE'
 ]
