087fa4e24ebf41ff9b746412627e9ccf7a0369dd
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-04-05 16:22:08 +0300

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,3 @@
-# Инициализируем logger для всех модулей routes
-import logging
-logger = logging.getLogger("1min-relay")
-
 # Импортируем всё содержимое модулей
 from .text import *
 from .images import *
