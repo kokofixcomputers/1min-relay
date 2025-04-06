eeee44bb80b2797b35d06eeb2252db0457ec38e4
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-04-06 20:34:06 +0300

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,11 +1,19 @@
 # routes/__init__.py
-# Импорт маршрутов
+# Инициализация пакета routes
 
-# Импортируем логгер
+# Импортируем необходимые модули
 from utils.logger import logger
 from utils.imports import *
 from utils.constants import *
-from utils.common import ERROR_HANDLER, handle_options_request, set_response_headers, create_session, api_request, safe_temp_file, calculate_token
+from utils.common import (
+    ERROR_HANDLER, 
+    handle_options_request, 
+    set_response_headers, 
+    create_session, 
+    api_request, 
+    safe_temp_file, 
+    calculate_token
+)
 
 # Делаем app и limiter доступными при импорте routes
 import sys
@@ -14,25 +22,19 @@ mod = sys.modules[__name__]
 # Импортируем app и limiter из корневого модуля
 try:
     import app as root_app
-    # Переносим объекты app и limiter в текущий модуль
+    # Переносим объекты в текущий модуль
     mod.app = root_app.app
     mod.limiter = root_app.limiter
-    # Добавляем IMAGE_CACHE для доступа из всех модулей маршрутов
     mod.IMAGE_CACHE = root_app.IMAGE_CACHE
-    # Добавляем MEMORY_STORAGE для работы с данными в памяти
     mod.MEMORY_STORAGE = root_app.MEMORY_STORAGE
-    # Также добавляем MAX_CACHE_SIZE, который может использоваться вместе с IMAGE_CACHE
-    mod.MAX_CACHE_SIZE = 100  # или другое подходящее значение
-    logger.info("Глобальные app, limiter, IMAGE_CACHE и MEMORY_STORAGE успешно переданы в модуль маршрутов")
+    mod.MAX_CACHE_SIZE = 100  # Максимальный размер кэша изображений
+    logger.info("Глобальные объекты успешно переданы в модуль маршрутов")
     
     # Импортируем все модули маршрутов
-    from . import files
-    from . import text
-    from . import images
-    from . import audio
+    from . import files, text, images, audio
     logger.info("Все модули маршрутов импортированы")
     
-    # Обеспечиваем прямой доступ к маршрутам из корневого модуля
+    # Обеспечиваем доступ к маршрутам из корневого модуля
     root_app.routes = mod
     logger.info("Модуль маршрутов добавлен в корневой модуль app")
     
