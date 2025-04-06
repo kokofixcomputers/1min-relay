222b1d4efc73b51ba0266b26ca9dffd244c74302
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-04-06 12:27:05 +0300

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,8 @@
-# Импортируем всё содержимое модулей
-from .text import *
-from .images import *
-from .audio import *
-from .files import *
+# routes/__init__.py
+# Полностью переписан для использования прямого импорта app и limiter из app.py
+
+# Импортируем логгер
+from utils.logger import logger
+from utils.imports import *
+
+logger.info("Инициализация маршрутов начата")
