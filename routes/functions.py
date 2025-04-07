8a922d450c3661fdc7300e150b4d429fdd8ceea6
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-04-07 21:51:31 +0300

    Update functions.py

--- routes/functions.py
+++ routes/functions.py
@@ -17,59 +17,19 @@ from utils.common import (
 from utils.memcached import safe_memcached_operation
 
 # Экспортируем общие функции
-from .functions.shared_func import (
-    validate_auth,
-    handle_api_error,
-    format_openai_response,
-    format_image_response,
-    stream_response,
-    get_full_url,
-    extract_data_from_api_response,
-    extract_text_from_response,
-    extract_image_urls,
-    extract_audio_url
-)
+from .functions.shared_func import *
 
 # Экспортируем функции для текстовых моделей
-from .functions.txt_func import (
-    format_conversation_history,
-    get_model_capabilities,
-    prepare_payload,
-    transform_response,
-    emulate_stream_response
-)
+from .functions.txt_func import *
 
 # Экспортируем функции для изображений
-from .functions.img_func import (
-    build_generation_payload,
-    parse_aspect_ratio,
-    create_image_variations,
-    process_image_tool_calls
-)
+from .functions.img_func import *
 
 # Экспортируем функции для аудио
-from .functions.audio_func import (
-    prepare_models_list,
-    prepare_whisper_payload,
-    prepare_tts_payload,
-    try_models_in_sequence,
-    upload_audio_file
-)
+from .functions.audio_func import *
 
 # Экспортируем функции для файлов
-from .functions.file_func import (
-    get_user_files,
-    save_user_files,
-    create_temp_file,
-    upload_asset,
-    get_mime_type,
-    retry_image_upload,
-    format_file_response,
-    create_api_response,
-    find_conversation_id,
-    find_file_by_id,
-    create_conversation_with_files
-)
+from .functions.file_func import *
 
 
 
