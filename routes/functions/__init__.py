8014bdcdedba039d0f2ba8bdb4bedb9061a64e82
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-04-07 21:47:00 +0300

    Update __init__.py

--- routes/functions/__init__.py
+++ routes/functions/__init__.py
@@ -1,9 +1,49 @@
 # routes/functions/__init__.py
 # Инициализация субпакета функций
 
-# Экспортируем все функции из модулей для удобства импорта
-from .shared_func import *
-from .txt_func import *
-from .img_func import *
-from .audio_func import *
-from .file_func import * 
+# Экспортируем общие функции
+from .shared_func import (
+    validate_auth,
+    handle_api_error,
+    format_openai_response,
+    format_image_response,
+    stream_response,
+    get_full_url,
+    extract_data_from_api_response,
+    extract_text_from_response,
+    extract_image_urls,
+    extract_audio_url
+)
+
+# Экспортируем функции для текстовых моделей
+from .txt_func import (
+    format_conversation_history,
+    get_model_capabilities,
+    prepare_payload,
+    transform_response,
+    emulate_stream_response
+)
+
+# Экспортируем функции для изображений
+from .img_func import (
+    build_generation_payload,
+    parse_aspect_ratio,
+    create_image_variations,
+    build_img2img_payload,
+    process_image_tool_calls
+)
+
+# Экспортируем функции для аудио
+from .audio_func import (
+    prepare_models_list,
+    prepare_whisper_payload,
+    prepare_tts_payload
+)
+
+# Экспортируем функции для файлов
+from .file_func import (
+    upload_asset,
+    get_mime_type,
+    retry_image_upload,
+    upload_audio_file
+) 
