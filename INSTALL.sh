c9472bf91eade66d5a929a6520678a319b272291
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-04-07 19:39:20 +0300

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ###################
-sudo apt update
-sudo apt install python3 python3-venv python3-pip -y
-sudo apt install memcached libmemcached-tools -y
-sudo systemctl enable memcached
-sudo systemctl start memcached
+# run with sudo
+apt update
+apt install python3 python3-venv python3-pip -y
+apt install memcached libmemcached-tools -y
+systemctl enable memcached
+systemctl start memcached
 ###################
 python3 -m venv venv
 source venv/bin/activate
